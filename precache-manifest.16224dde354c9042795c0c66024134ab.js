a748d46224f63faaa02c4ad035b4ab62771fbb78
Curtis Noonan <curtis.noonan@gmail.com>
2020-03-04 19:35:29 -0800

    Updates

---
+++ precache-manifest.16224dde354c9042795c0c66024134ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06961d8f2120577a269e072a135d412",
+    "url": "/hiking-cards-list/index.html"
+  },
+  {
+    "revision": "af8aea129801b2b74896",
+    "url": "/hiking-cards-list/static/css/main.efd8e682.chunk.css"
+  },
+  {
+    "revision": "1d24c1f657a6f9a79f44",
+    "url": "/hiking-cards-list/static/js/2.0dcbddb6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/hiking-cards-list/static/js/2.0dcbddb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8aea129801b2b74896",
+    "url": "/hiking-cards-list/static/js/main.b1be1f8f.chunk.js"
+  },
+  {
+    "revision": "422a4f4ffbff2a2de24f",
+    "url": "/hiking-cards-list/static/js/runtime-main.f321a01d.js"
+  }
+]);
