6c2cda376a6d352352783707fdd9ed708b534cb3
Curtis Noonan <curtis.noonan@gmail.com>
2020-03-05 16:45:23 -0800

    added labels for card values

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,5 +7,9 @@ export const Card = props => (
     <img alt={props.trail.name}
       src={props.trail.imageurl}/>
         <h2>{props.trail.name}</h2>
+        <h3>{props.trail.location}</h3>
+        <h3>{props.trail.length}</h3>
+        <h3>Elevation Change: {props.trail.elevation_change}</h3>
+        <h3>Approx hiking time: {props.trail.hiking_time}</h3>
     </div>
 );
